70ebb62e11226f12d5dc85e0514dbafcd9c23ee8
Ankit Gandotra <ankitgandotra827@gmail.com>
2023-03-10 17:37:31 +0530

    types nd operators

--- 05/demos/before/js-demo/home.js
+++ 05/demos/before/js-demo/home.js
@@ -1,7 +1,45 @@
-
-
-
 let price = 25;
+price = 49.0; //cant be redeclare
+//let welcome = "welcome";
+// const price = 10;
+// price = 1200; // const cant be changed
+
+//var price=90;
+price = price + 1;
+//+,*,/,%,++,--
+
+//  let message = "hello";
+//  message.toUpperCase();
+//  message.substring(1);
+// message = message + "world"; //concatnitaion
+
+//string methods
+
+// let amount =123;
+// amount = amount.toString(); //converting to string datatype
+// showMessage(amount)
+
+// let amount = Number.parseFloat("123.12");
+// let amount = Number.parseFloat("A123.12");//NaN
+// let amount = Number.parseFloat("123.12A");//can be parsed
+
+//let saved =true;//boolean variable
+//saved=!saved;  //inverting boolean variable
+
+//null and undefined
+//let save;        --undefined
+//let save =null    --null
 
 showMessage(price);
+// showMessage(welcome);
+//alert("i will make 25 to changes");
+showMessage('"Ankit making the changes..."');
+//showMessage("changes...");
+// let 99time=99;
+// console.log(99time);
+//let let=20; // keyword cant be the variable name
+
 console.log(price);
+//console.log(welcome);
+alert("this is mesaage from home JS");
+// ankit is making comment section Headers..!!
